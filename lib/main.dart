2335f4a3779fbc7abddd0e4698c5f7741dd3d28b
si3aa <mahmoud22said22@gmail.com>
2024-06-27 12:39:16 +0300

    create customSearchWidget and edit NotesView

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class NoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark
       ),
