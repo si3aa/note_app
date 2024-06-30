8190a74260ffc1669ae61f2bae8eb4ff06d08f46
si3aa <mahmoud22said22@gmail.com>
2024-06-30 14:50:33 +0300

    build note_item_widget and add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class NoteApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        brightness: Brightness.dark
+        brightness: Brightness.dark,
+        fontFamily: 'Poppins'
       ),
       home: const NotesView(),
     );
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -14,6 +14,42 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.yellow,
+      ),
+      child: Column(
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: const Text(
+              'Build Your Application',
+              style: TextStyle(color: Colors.black),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.black,
+              ),
+            ),
+          )
         ],
       ),
     );
