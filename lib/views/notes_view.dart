233eb78a9d65df55792822f0e5a151451f8510b1
si3aa <mahmoud22said22@gmail.com>
2024-07-03 14:58:11 +0300

    add border to bottomSheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:note_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -9,8 +10,12 @@ class NotesView extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showModalBottomSheet(context: context, builder: (context){
-            return Container() ;
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16)
+            ),
+            context: context, builder: (context){
+            return const AddNoteBottomSheet() ;
           });
         },
         child: const Icon(Icons.add),
