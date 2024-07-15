e4fc68a7544d8b84bd2be5a9222c5576fec6acd3
si3aa <mahmoud22said22@gmail.com>
2024-07-15 16:30:00 +0300

    display notes data

--- lib/cubit/notes_cubit/notes_cubit_state.dart
+++ lib/cubit/notes_cubit/notes_cubit_state.dart
@@ -5,16 +5,5 @@ abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
 
-class NotesLoading extends NotesState {}
 
-class NotesSuccess extends NotesState {
-  final List<NoteModel> notes;
 
-  NotesSuccess(this.notes);
-}
-
-class NotesFailure extends NotesState {
-  final String errorMessage;
-
-  NotesFailure(this.errorMessage);
-}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubit/notes_cubit/notes_cubit_cubit.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
 import 'package:note_app/views/widgets/notes_list_view.dart';
 
-class NotesViewBody extends StatelessWidget {
+class NotesViewBody extends StatefulWidget {
   static const String routeName = "home";
   const NotesViewBody({super.key});
 
+  @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return const Padding(
@@ -16,7 +28,9 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(title: 'Notes', icon: Icons.search),
-          Expanded(child: NotesListView())
+          Expanded(
+            child: NotesListView(),
+          )
         ],
       ),
     );
